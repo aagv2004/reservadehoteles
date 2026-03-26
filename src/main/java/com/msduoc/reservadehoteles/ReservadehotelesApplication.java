98eedae37f36f38e1a816bb21b1cff67230b26a2
Alejandro Gonzalez Vergara <andresg.vergara04@gmail.com>
2026-03-26 12:36:28 -0300

    First Commit

---
+++ src/main/java/com/msduoc/reservadehoteles/ReservadehotelesApplication.java
@@ -0,0 +1,13 @@
+package com.msduoc.reservadehoteles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservadehotelesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservadehotelesApplication.class, args);
+	}
+
+}
