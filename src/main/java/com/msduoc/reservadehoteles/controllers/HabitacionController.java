564b1961f831dbe867547c6ebe2c792f84937509
Alejandro Gonzalez Vergara <andresg.vergara04@gmail.com>
2026-03-28 00:18:10 -0300

    optimizar inicialización de datos usando List.of

--- src/main/java/com/msduoc/reservadehoteles/controllers/HabitacionController.java
+++ src/main/java/com/msduoc/reservadehoteles/controllers/HabitacionController.java
@@ -25,61 +25,39 @@ public class HabitacionController {
 
     // Poblamos esa "tabla"
     public HabitacionController() {
-        List<Reserva> reservasHabitacion201 = new ArrayList<>();
-        List<Reserva> reservasHabitacion202 = new ArrayList<>();
-        List<Reserva> reservasHabitacion203 = new ArrayList<>();
-        List<Reserva> reservasHabitacion204 = new ArrayList<>();
-        List<Reserva> reservasHabitacion205 = new ArrayList<>();
-        List<Reserva> reservasHabitacion206 = new ArrayList<>();
-        List<Reserva> reservasHabitacion207 = new ArrayList<>();
-        List<Reserva> reservasHabitacion208 = new ArrayList<>();
+        // 1. Poblado de información: Al igual que con los packs de mascotas, 
+        // aquí definimos los clientes una sola vez para reutilizarlos en las reservas.
+        Cliente c1 = new Cliente(1, "Alejandro", "Gonzalez", 22);
+        Cliente c2 = new Cliente(2, "Pedro", "Gonzalez", 45);
+        Cliente c3 = new Cliente(3, "Rodrigo", "Vera", 25);
+        Cliente c4 = new Cliente(4, "Eduardo", "Vergara", 32);
+        Cliente c5 = new Cliente(5, "Juan", "Valdez", 33);
 
-        // Habitaciones disponibles pares de 200, 202, 204, 206, 208
-        reservasHabitacion201.add(new Reserva(
-            1, 
-            EstadoReserva.PENDIENTE, 
-            "01/01/2024", 
-            "05/02/2024", 
-            "12/12/2023", 
-            new Cliente(1, "Alejandro", "Gonzalez", 22)));
-        reservasHabitacion203.add(new Reserva(
-            2, 
-            EstadoReserva.COMPLETADA, 
-            "07/02/2024", 
-            "12/02/2024", 
-            "06/02/2024",
-            new Cliente(2, "Pedro", "Gonzalez", 45)));
-        reservasHabitacion205.add(new Reserva(
-            3, 
-            EstadoReserva.CONFIRMADA, 
-            "02/01/2025", 
-            "06/01/2025", 
-            "25/12/2024", 
-            new Cliente(3, "Rodrigo", "Vera", 25)));
-        reservasHabitacion207.add(new Reserva(
-            4, 
-            EstadoReserva.PENDIENTE, 
-            "05/10/2025", 
-            "09/10/2025", 
-            "25/09/2025",
-            new Cliente(4, "Eduardo", "Vergara", 32)));
-        reservasHabitacion208.add(new Reserva(
-            5, 
-            EstadoReserva.CANCELADA, 
-            "01/01/2022", 
-            "02/01/2022", 
-            "29/12/2021", 
-            new Cliente(5, "Juan", "Valdez", 33)));
+        // Crear reservas de habitaciones
+       
 
 
-        habitaciones.add(new Habitacion(1, 201, TipoHabitacion.SIMPLE, 5000,reservasHabitacion201));
-        habitaciones.add(new Habitacion(2, 202, TipoHabitacion.DOBLE, 10000, reservasHabitacion202));
-        habitaciones.add(new Habitacion(3, 203, TipoHabitacion.SUITE, 15000, reservasHabitacion203));
-        habitaciones.add(new Habitacion(4, 204, TipoHabitacion.SIMPLE, 5000, reservasHabitacion204));
-        habitaciones.add(new Habitacion(5, 205, TipoHabitacion.SUITE, 15000, reservasHabitacion205));
-        habitaciones.add(new Habitacion(6, 206, TipoHabitacion.DOBLE, 10000, reservasHabitacion206));
-        habitaciones.add(new Habitacion(7, 207, TipoHabitacion.SUITE, 15000, reservasHabitacion207));
-        habitaciones.add(new Habitacion(8, 208, TipoHabitacion.SIMPLE, 5000, reservasHabitacion208));
+        habitaciones.add(new Habitacion(1, 201, TipoHabitacion.SIMPLE, 5000, List.of(
+        new Reserva(1, EstadoReserva.PENDIENTE, "01/01/2024", "05/02/2024", "12/12/2023", c1))));
+
+        habitaciones.add(new Habitacion(2, 202, TipoHabitacion.DOBLE, 10000, List.of())); // Sin reservas
+
+        habitaciones.add(new Habitacion(3, 203, TipoHabitacion.SUITE, 15000, List.of(
+            new Reserva(2, EstadoReserva.COMPLETADA, "07/02/2024", "12/02/2024", "06/02/2024", c2)
+        )));
+
+        habitaciones.add(new Habitacion(4, 204, TipoHabitacion.SIMPLE, 5000, List.of())); // Sin reservas
+
+        habitaciones.add(new Habitacion(5, 205, TipoHabitacion.SUITE, 15000, List.of(
+        new Reserva(3, EstadoReserva.CONFIRMADA, "02/01/2025", "06/01/2025", "25/12/2024", c3))));
+
+        habitaciones.add(new Habitacion(6, 206, TipoHabitacion.DOBLE, 10000, List.of())); // Sin reservas
+
+        habitaciones.add(new Habitacion(7, 207, TipoHabitacion.SUITE, 15000, List.of(
+        new Reserva(4, EstadoReserva.PENDIENTE, "05/10/2025", "09/10/2025", "25/09/2025", c4))));
+
+        habitaciones.add(new Habitacion(8, 208, TipoHabitacion.SIMPLE, 5000, List.of(
+        new Reserva(5, EstadoReserva.CANCELADA, "01/01/2022", "02/01/2022", "29/12/2021", c5))));
     }
 
     // Comparar fechas para ver si están disponibles las habitaciones:
